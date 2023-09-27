2f073ba7ae8d91d6ae7e37919e2525a6c54ca491
Pensee Elessawy <117679065+penseeelessawy@users.noreply.github.com>
2023-09-27 20:44:43 +0300

    Add files via upload

---
+++ Systick/TASK_6/SYSTICK_program.c
@@ -0,0 +1,144 @@
+/*
+ * SYS_TICK_program.c
+ *
+ *  Created on: Aug 10, 2022
+ *      Author: Omar Gamal
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "SYSTICK_interface.h"
+#include "SYSTICK_config.h"
+#include "SYSTICK_private.h"
+
+
+#include "GPIO_interface.h"
+
+
+void (*CallBack)(void) = (void *) 0;
+u8 IntervalState = SYSTICK_PERIODIC_INTERVAL;
+u32 Current_value ;
+
+/*
+ * STK_voidInit
+ * description: Selects the clock source of the SysTick (AHB, AHB/8)
+ */
+void STK_voidInit(u8 Copy_u8ClkSRC){
+
+	switch(Copy_u8ClkSRC)
+	{
+	case SYSTICK_AHB_8:
+		CLR_BIT(SYSTICK->CTRL,2);
+		break;
+	case SYSTICK_AHB:
+		SET_BIT(SYSTICK->CTRL,2);
+		break;
+	}
+
+}
+
+
+/*
+ * STK_voidStopTimer
+ * description: Stops the Timer
+ */
+void STK_voidStopTimer(void){
+	Current_value = SYSTICK->VAL ;
+	SYSTICK->LOAD = 0x00000000;
+	SYSTICK->VAL = 0;
+	CLR_BIT(SYSTICK->CTRL, 0);
+}
+
+
+
+/*
+ * STK_voidSetBusyWait
+ * description: Starts a synchronous wait (delay)
+ */
+void STK_voidSetBusyWait(u32 Copy_u32TickCount){
+
+	/* Disable the systick interrupt
+	 * Set LOAD value
+	 * start the timer
+	 * empty loop until the flag is raised
+	 * reset the flag
+	 * Reset the LOAD register
+	 * stop the timer
+	 */
+	CLR_BIT(SYSTICK->CTRL, 1);
+	SYSTICK->LOAD = (Copy_u32TickCount - 1) & 0x00FFFFFF;
+	SYSTICK->VAL = 0;
+	SET_BIT(SYSTICK->CTRL, 0);
+	while(!(GET_BIT(SYSTICK->CTRL, 16))){
+		__asm("NOP");}
+	CLR_BIT(SYSTICK->CTRL, 0);
+	SYSTICK->VAL = 0;
+}
+
+
+/*
+ * STK_voidSetPeriodicInterval
+ * description: Starts a periodic interval asynchronous wait
+ */
+void STK_voidSetPeriodicInterval(u32 Copy_u32TickCount, void (*ptr)(void)){
+
+	/* Set LOAD value
+	 * assign the callback function to the handler
+	 * Enable the interrupt
+	 * start the timer
+	 */
+	IntervalState = SYSTICK_PERIODIC_INTERVAL;
+	SYSTICK->LOAD = (Copy_u32TickCount - 1) & 0x00FFFFFF;
+	CallBack = ptr;
+	SET_BIT(SYSTICK->CTRL, 1);
+	SET_BIT(SYSTICK->CTRL, 0);
+}
+
+
+/*
+ * STK_voidSetSingleInterval
+ * description: Starts a single interval asynchronous wait
+ */
+void STK_voidSetSingleInterval(u32 Copy_u32TickCount, void (*ptr)(void)){
+
+	/* Set LOAD value
+	 * assign the callback function to the handler
+	 * Enable the interrupt
+	 * start the timer
+	 */
+
+	IntervalState = SYSTICK_SINGLE_INTERVAL;
+	SYSTICK->LOAD = (Copy_u32TickCount - 1) & 0x00FFFFFF;
+	CallBack = ptr;
+	SET_BIT(SYSTICK->CTRL, 1);
+	SET_BIT(SYSTICK->CTRL, 0);
+}
+
+
+/*
+ * STK_u32GetRemainingTicks
+ * description: Gets the number of ticks remaining until we reach zero
+ */
+u32 STK_u32GetRemainingTicks(void){
+	return SYSTICK->VAL ;
+}
+/*
+ * STK_u32GetElapsedTicks
+ * description: Gets the number of ticks elapsed since we started counting
+ */
+u32 STK_u32GetElapsedTicks(void){
+	return (SYSTICK->LOAD -  SYSTICK->VAL) ;
+
+}
+
+void SysTick_Handler(void){
+	CallBack();
+	if(IntervalState == SYSTICK_PERIODIC_INTERVAL){
+
+	}else{
+		CLR_BIT(SYSTICK->CTRL, 0);
+	}
+	SYSTICK->VAL = 0; // Clearing the flag
+}
